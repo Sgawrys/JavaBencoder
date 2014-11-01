d5122c300d8f1fa8500b565f5391f909aab2c02c
Stefan Gawrys <sgawrys@ic.sunysb.edu>
2014-11-01 03:58:16 -0400

    Added some more work on decoder for bencoding

--- src/com/sgawrys/bencoder/Decoder.java
+++ src/com/sgawrys/bencoder/Decoder.java
@@ -1,5 +1,12 @@
 package com.sgawrys.bencoder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sgawrys.bencoder.exceptions.DecoderException;
+
 /**
  * Decoder for bencoded strings.
  * 
@@ -10,6 +17,13 @@ public class Decoder {
 
 	private static final Decoder DECODER = new Decoder();
 	
+	private static final String START_TOKEN = "i";
+	private static final String START_DICT_TOKEN = "d";
+	private static final String START_LIST_TOKEN = "l";
+	
+	private static final String END_TOKEN = "e";
+	private static final String COLON_TOKEN = ":";
+	
 	private Decoder() {
 		
 	}
@@ -19,6 +33,51 @@ public class Decoder {
 	}
 	
 	public Object decode(String bencodedString) {
+		return determineType(bencodedString);
+	}
+	
+	public Object determineType(String bencodedString) {
+		if(bencodedString.startsWith(START_LIST_TOKEN)) {
+			return decodeList(bencodedString);
+		}
+		
+		if(bencodedString.startsWith(START_DICT_TOKEN)) {
+			return decodeDict(bencodedString);
+		}
+		
+		if(bencodedString.startsWith(START_TOKEN)) {
+			return decodeInt(bencodedString);
+		}
+		
+		return null;
+	}
+	
+	public List<?> decodeList(String encodedList) {
+		List<Object> decodedList = new ArrayList<Object>();
+		
+		return decodedList;
+	}
+	
+	public Map<?,?> decodeDict(String encodedMap) {
+		Map<Object, Object> decodedMap = new HashMap<Object, Object>();
+		
+		return decodedMap;
+	}
+	
+	public Integer decodeInt(String encodedInt) {
+		Integer decodedInt = Integer.parseInt(encodedInt.substring(1, encodedInt.length() - 1));
+		return decodedInt;
+	}
+	
+	public String decodeString(String encodedString) throws DecoderException {
+		int colonTokenIndex = encodedString.indexOf(COLON_TOKEN);
+		
+		if(colonTokenIndex == -1)
+			throw new DecoderException();
+		
+		String extractedString = encodedString.substring(colonTokenIndex + 1);
+		
+		return extractedString;
 	}
 	
 }
--- src/com/sgawrys/bencoder/tests/DecoderTest.java
+++ src/com/sgawrys/bencoder/tests/DecoderTest.java
@@ -4,11 +4,40 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.sgawrys.bencoder.Decoder;
+import com.sgawrys.bencoder.exceptions.DecoderException;
+
 public class DecoderTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIntegerDecode() {
+		String bencodedInteger = "i42e";
+		
+		Integer decodedInteger =  Decoder.getInstance().decodeInt(bencodedInteger);
+		Integer testInteger = new Integer(42);
+		
+		assertEquals(testInteger, decodedInteger);
+		
+		String negativeBencodedInteger = "i-42e";
+		
+		Integer negativeDecoded = Decoder.getInstance().decodeInt(negativeBencodedInteger);
+		Integer testInteger2 = new Integer(-42);
+		
+		assertEquals(testInteger2, negativeDecoded);
+	}
+	
+	@Test
+	public void testStringDecode() throws DecoderException {
+		String test = "4:test";
+		String result = Decoder.getInstance().decodeString(test);
+		
+		assertEquals("test", result);
+	}
+	
+	@Test(expected = DecoderException.class)
+	public void testStringDecode_2() throws DecoderException {
+		String test = "3435353";
+		String result = Decoder.getInstance().decodeString(test);
 	}
 
 }
